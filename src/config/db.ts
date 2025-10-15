afee1095843c457b6a81eb8b2b1c51bed0ab0470
samuel <samuelolagoroye6@gmail.com>
2025-10-15 14:58:41 +0100

    Setup turso db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,3 +1,14 @@
 // import { PrismaClient } from "../../prisma/generated/prisma/client.js";
+
+// import { PrismaClient } from "@prisma/client";
+// export const prisma = new PrismaClient();
+
 import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+
+const adapter = new PrismaLibSQL({
+  url: process.env.TURSO_DATABASE_URL as string,
+  authToken: process.env.TURSO_AUTH_TOKEN as string,
+});
+
+export const prisma = new PrismaClient({ adapter });
